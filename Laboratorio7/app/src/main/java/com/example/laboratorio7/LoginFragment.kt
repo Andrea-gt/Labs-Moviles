0403ea999941c65c72932e1608cb8c9f6c92e578
Andrea-gt <a.ximeram@gmail.com>
2022-08-28 20:36:15 -0600

    Avances de Laboratorio 7

---
+++ Laboratorio7/app/src/main/java/com/example/laboratorio7/LoginFragment.kt
@@ -0,0 +1,38 @@
+package com.example.laboratorio7
+
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.TextView
+import androidx.core.os.bundleOf
+import androidx.fragment.app.Fragment
+import androidx.navigation.findNavController
+import com.google.android.material.textfield.TextInputEditText
+import com.google.android.material.textfield.TextInputLayout
+
+class LoginFragment : Fragment(R.layout.fragment_login){
+    private lateinit var buttonLogin: Button
+    private lateinit var buttonCreate: TextView
+    private lateinit var emailInput: TextInputEditText
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        buttonLogin = view.findViewById(R.id.LoginButton)
+        buttonCreate = view.findViewById(R.id.mainactivity_NeedUserText)
+        emailInput = view.findViewById(R.id.mainInput_EditText)
+        setListeners()
+    }
+
+    private fun setListeners() {
+        buttonLogin.setOnClickListener {
+            val action = LoginFragmentDirections.actionLoginFragmentToHomeFragment(
+                userEmail = emailInput.editableText!!.toString()
+            )
+            requireView().findNavController().navigate(action)
+        }
+        buttonCreate.setOnClickListener {
+            requireView().findNavController().navigate(R.id.action_loginFragment_to_newAccountFragment)
+        }
+    }
+}
